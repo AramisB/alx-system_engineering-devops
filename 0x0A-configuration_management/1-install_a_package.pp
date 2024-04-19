3778370ca1642dfe462dd4a7138617ec173d2ee9
AramisB <brendachem7@gmail.com>
2024-04-20 01:32:18 +0300

    A Puppet file that creates a manifest that kills a process named killmenow

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # install flask from pip3
+package { 'werkzeug':
+    ensure   => '2.0.2',
+    provider => 'pip',
+}
+
 package { 'flask':
     ensure   => '2.1.0',
-    provider => 'pip3',
+    provider => 'pip',
 }
