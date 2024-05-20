e3f0f8eb89011ede488baf615d36df978e9d6070
AramisB <brendachem7@gmail.com>
2024-05-21 02:32:03 +0300

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A script that exports data to json
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    employee_url = f'{base_url}/users/{employee_id}'
+    response = requests.get(employee_url)
+    employee = response.json()
+
+    todos_url = f'{base_url}/todos?userId={employee_id}'
+    response = requests.get(todos_url)
+    todos = response.json()
+
+    employee_name = employee['name']
+    json_filename = f"{employee_id}.json"
+    dict_data = {employee_id: []}
+    for task in todos:
+        dict_data[employee_id].append({
+            "title": task["title"],
+            "completed": task["completed"],
+            "username": employee_name,
+            })
+    with open(json_filename, 'w') as f:
+        json.dump(dict_data, f)
